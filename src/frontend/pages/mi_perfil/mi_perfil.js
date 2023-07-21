ff17e08c0bf5c98e4e4c81ccfc1b5bcc3065576f
Kamiones <carlosquinonescosta2003@gmail.com>
2023-07-20 23:50:02 -0500

    20/07

--- src/frontend/pages/mi_perfil/mi_perfil.js
+++ src/frontend/pages/mi_perfil/mi_perfil.js
@@ -1,31 +1,137 @@
 import {useState, useEffect} from 'react';
 import './mi_perfil.css';
-import './tabs.css';
 import './f-input.css';
+import './tabs.css';
 
 function MiPerfil() {
-  const [toggleState, setToggleState] = useState(1);
+  var id = 1;
 
-  function procesardatos(data){
-    document.getElementById('nombres').value = data.nombres;
-    document.getElementById('rol').value = data.rol;
-    document.getElementById('apellidos').value = data.apellidos;
-    document.getElementById('tipo').value = data.tipo_doc;
-    document.getElementById('numero').value = data.numero_doc;
+  const [usuario, setUsuario] = useState({});
+  const [universidades, setUniversidades] = useState([]);
+  const [carreras, setCarreras] = useState([]);
+  const [cursos, setCursos] = useState([]);
+
+  const [universidadState, setUniversidadState] = useState(false);
+  const [carreraState, setCarreraState] = useState(false);
+
+  function checkStatus(response) {
+    if (!response.ok) {
+      throw Error("Error en la solicitud: " + response.statusText);
+    }
+    return response;
   }
-  
 
-  
+  function handleError(err) {
+    alert("Error en la solicitud: " + err);
+  }
+
+  // Cargar datos del usuario
+  function cargarUsuario(data) {
+    document.getElementById("nombres").value = data.nombres;
+    document.getElementById("apellidos").value = data.apellidos;
+    document.getElementById("tipo").value = data.tipo_doc;
+    document.getElementById("numero").value = data.numero_doc;
+    document.getElementById("rol1").value = data.rol;
+    document.getElementById("usuario").value = data.username;
+    document.getElementById("titulo").value  = data.titulo;
+    document.getElementById("presentacion").value = data.presentacion;
+    document.getElementById("carrera").value = data.idCarrera;
+    document.getElementById("universidad").value = data.idUniversidad;
+
+    data.idUniversidad ? setUniversidadState(true) : setUniversidadState(false)
+    data.idCarrera ? setCarreraState(true) : setCarreraState(false)
+
+    setUsuario(data)
+  }
+
   useEffect(() => {
-    
-      const id = 1;
-      fetch(`https://pw-tidb-server-production.up.railway.app/mostrar-usuarios/${id}`)
-        .then(response => response.json())
-        .then(procesardatos) // Corrected this line
-        .catch(e => console.log("Ocurrio un error " + e));
-    
+    fetch(`http://localhost:3001/mi-perfil/cargar/usuario/${id}`)
+      .then(checkStatus)
+      .then(response => response.json())
+      .then(cargarUsuario)
+      .catch(handleError)
 
+    fetch("http://localhost:3001/mi-perfil/cargar/universidades")
+      .then(checkStatus)  
+      .then(response => response.json())
+      .then(data => setUniversidades(data))
+      .catch(handleError)
+
+    fetch(`http://localhost:3001/mi-perfil/cargar/carreras/1`)
+      .then(checkStatus)  
+      .then(response => response.json())
+      .then(data => setCarreras(data))
+      .catch(handleError)
+    
+    fetch("http://localhost:3001/mi-perfil/cargar/cursos/1")
+      .then(checkStatus)  
+      .then(response => response.json())
+      .then(data => setCursos(data.Cursos))
+      .catch(handleError)
   }, []);
+  //
+
+  // Guardar datos del usuario
+  var password = usuario.password;
+
+  function postGuardar(password) {
+    var nombres = document.getElementById("nombres").value;
+    var apellidos = document.getElementById("apellidos").value;
+    var tipo = document.getElementById("tipo").value;
+    var numero = document.getElementById("numero").value;
+    var rol = document.getElementById("rol1").value;
+    var titulo = document.getElementById("titulo").value;
+    var presentacion = document.getElementById("presentacion").value;
+    var carrera = document.getElementById("carrera").value;
+    var universidad = document.getElementById("universidad").value;
+
+    fetch(`http://localhost:3001/mi-perfil/guardar/${id}/${password}/${nombres}/${apellidos}/${tipo}/${numero}/${rol}/${titulo}/${presentacion}/${carrera}/${universidad}`)
+      .then(checkStatus)
+      .then(response => response.text())
+      .then(data => alert(data))
+      .catch(handleError)
+  }
+
+  function guardar() {
+    var actual = document.getElementById("actual").value; 
+    var nueva = document.getElementById("nueva").value;
+    var repetir = document.getElementById("repetir").value;
+    
+    if(actual !== "" || nueva !== "" || repetir !== "") {
+      if(actual === password) {
+        if(nueva !== "") {
+          if(nueva === repetir) {
+            postGuardar(nueva);
+          }
+          else {
+            alert("Las dos contraseñas no coinciden.")
+          }
+        }
+        else {
+          alert("La nueva contraseña no puede estar vacía.")
+        }
+      }
+      else {
+        alert("La contraseña actual ingresada es incorrecta.")
+      }
+    }
+    else {
+      postGuardar(password);
+    }
+  }
+  //
+
+  function guardarFoto() {
+    var link = document.getElementById("link").value
+
+    fetch(`/mi-perfil/guardar/foto/${id}/${link}`)
+      .then(checkStatus)  
+      .then(response => response.text())
+      .then(data => alert(data))
+      .catch(handleError)
+  }
+
+  const [toggleState, setToggleState] = useState(1);
 
   const toggleTab = (index) => {
     setToggleState(index);
@@ -35,58 +141,48 @@ function MiPerfil() {
     <div id="mi_perfil">
       <div className="container-1">
         <p className="p-title">Mi perfil</p>
-        <button id="btn-guardar">Guardar</button>
+        <button id="btn-guardar" onClick={guardar}>Guardar</button>
         <hr className="hr"></hr>
-        <p className="p">Información personal</p>
+        <p className="p">Información personal</p>       
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <input type="text" id="nombres" className="f-input"/>
+                <label for="nombres" className="f-label">Nombres</label>
+                <br/>
+                <select id="rol1" className="f-input">
+                  <option value="false">Estudiante</option>
+                  <option value="true">Profesor</option>
+                </select>
+                <label for="rol1" className="f-label">Rol</label>
+              </td>
 
+              <td className="second-column">
+                <input type="text" id="apellidos" className="f-input"/>
+                <label for="apellidos" className="f-label">Apellidos</label>
+              </td>
 
-        
-  <table>
-    <tbody>
-      <tr>
-        <td>
-          <input type="text" id="nombres" name="nombres" className="f-input" />
-          <label htmlFor="nombres" className="f-label">
-            Nombres
-          </label>
-          <br />
-          <input type="text" id="rol" name="rol" className="f-input" />
-          <label htmlFor="rol" className="f-label">
-            Rol
-          </label>
-        </td>
+              <td>
+                <select id="tipo" className="f-input">
+                  <option value="false">DNI</option>
+                  <option value="true">Carnet de extranjería</option>
+                </select>
+                <label for="tipo" className="f-label">Tipo de documento</label>
+                <br />
+                <input type="text" id="numero" className="f-input"/>
+                <label for="numero" className="f-label">Número</label>
+              </td>
 
-        <td className="second-column">
-          <input type="text" id="apellidos" name="apellidos" className="f-input" />
-          <label htmlFor="apellidos" className="f-label">
-            Apellidos
-          </label>
-        </td>
-
-        <td>
-          <input type="text" id="tipo" name="tipo" className="f-input"  />
-          <label htmlFor="tipo" className="f-label">
-            Tipo de documento
-          </label>
-          <br />
-          <input type="text" id="numero" name="numero" className="f-input"  />
-          <label htmlFor="numero" className="f-label">
-            Número
-          </label>
-        </td>
-
-        <td className="fourth-column">
-          <img id="foto-usuario" src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png" alt="foto" />
-          <br />
-          <p id="adjuntar-foto">Adjuntar foto</p>
-        </td>
-      </tr>
-    </tbody>
-  </table>
-
-
-
-        
+              <td className="fourth-column">
+                <img id="foto-usuario" src={usuario.foto} alt="foto" />
+                <br />
+                <p id="adjuntar-foto" onClick={guardarFoto}>Adjuntar foto</p>
+                <input type="text" id="link" className="f-input"/>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
 
       <div className="container-2">
@@ -115,7 +211,7 @@ function MiPerfil() {
           <div
             className={toggleState === 1 ? "content  active-content" : "content"}
           >
-            <input type="text" id="usuario" className="f-input"/>
+            <input type="text" id="usuario" className="f-input" readOnly/>
             <label for="usuario" className="f-label">Usuario</label>
 
             <br/>
@@ -137,19 +233,34 @@ function MiPerfil() {
             <tbody>
             <tr>
               <td>
-                <input type="text" id="universidad" className="f-input f-input-search"/>
+                <select id="universidad" className="f-input f-input-search" disabled={universidadState}>
+                  <option selected value='null'>-- Seleccione una universidad --</option>
+                  {universidades.map(universidad => (
+                    <option value={universidad.id}>{universidad.nombre}</option>
+                  ))}
+                </select>
                 <label for="universidad" className="f-label f-label-search">Universidad</label>
                 <svg className="search-icon" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"> <path fill-rule="evenodd" clip-rule="evenodd" d="M11.76 10.27L17.49 16L16 17.49L10.27 11.76C9.2 12.53 7.91 13 6.5 13C2.91 13 0 10.09 0 6.5C0 2.91 2.91 0 6.5 0C10.09 0 13 2.91 13 6.5C13 7.91 12.53 9.2 11.76 10.27ZM6.5 2C4.01 2 2 4.01 2 6.5C2 8.99 4.01 11 6.5 11C8.99 11 11 8.99 11 6.5C11 4.01 8.99 2 6.5 2Z" fill="black"/> </svg>
 
                 <br/>
 
-                <input type="text" id="carrera" className="f-input f-input-search"/>
+                <select id="carrera" className="f-input f-input-search" disabled={carreraState}>
+                  <option selected value='null'>-- Seleccione una carrera --</option>
+                  {carreras.map(carrera => (
+                    <option value={carrera.id}>{carrera.nombre}</option>
+                  ))}
+                </select>
                 <label for="carrera" className="f-label  f-label-search">Carrera</label>
                 <svg className="search-icon" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"> <path fill-rule="evenodd" clip-rule="evenodd" d="M11.76 10.27L17.49 16L16 17.49L10.27 11.76C9.2 12.53 7.91 13 6.5 13C2.91 13 0 10.09 0 6.5C0 2.91 2.91 0 6.5 0C10.09 0 13 2.91 13 6.5C13 7.91 12.53 9.2 11.76 10.27ZM6.5 2C4.01 2 2 4.01 2 6.5C2 8.99 4.01 11 6.5 11C8.99 11 11 8.99 11 6.5C11 4.01 8.99 2 6.5 2Z" fill="black"/> </svg>
               </td>
 
               <td className="second-column">
-                <input type="text" id="cursos" className="f-input f-input-search"/>
+                <select id="cursos" className="f-input f-input-search">
+                  <option selected value='null'>-- Seleccione un curso --</option>
+                  {cursos.map(curso => (
+                    <option value={curso.id}>{curso.nombre}</option>
+                  ))}
+                </select>
                 <label for="cursos" className="f-label  f-label-search">Agregar cursos</label>
                 <svg className="search-icon" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"> <path fill-rule="evenodd" clip-rule="evenodd" d="M11.76 10.27L17.49 16L16 17.49L10.27 11.76C9.2 12.53 7.91 13 6.5 13C2.91 13 0 10.09 0 6.5C0 2.91 2.91 0 6.5 0C10.09 0 13 2.91 13 6.5C13 7.91 12.53 9.2 11.76 10.27ZM6.5 2C4.01 2 2 4.01 2 6.5C2 8.99 4.01 11 6.5 11C8.99 11 11 8.99 11 6.5C11 4.01 8.99 2 6.5 2Z" fill="black"/> </svg>
               </td>
@@ -161,7 +272,11 @@ function MiPerfil() {
           <div
             className={toggleState === 3 ? "content  active-content" : "content"}
           >
-            
+            <input type="text" id="titulo" className="f-input-pres"/>
+            <label for="titulo" className="f-label-pres">Título</label>
+            <br/>
+            <input type="text" id="presentacion" className="f-input-pres"/>
+            <label for="presentacion" className="f-label-pres">Presentación</label>
           </div>
         </div>
       </div>
--- src/frontend/pages/mis_horarios/mis_horarios.js
+++ src/frontend/pages/mis_horarios/mis_horarios.js
@@ -1,38 +1,120 @@
 import './mis_horarios.css'
 import './f-input.css'
+import {useState, useEffect} from 'react';
 
-function MisHorarios() {
+    function MisHorarios() {
+    var id = 1;
+    
+    const [horarios, setHorarios] = useState([]);
+
+    function checkStatus(response) {
+        if (!response.ok) {
+            throw Error("Error en la solicitud: " + response.statusText);
+        }
+        return response;
+        }
+    
+    function handleError(err) {
+        alert("Error en la solicitud: " + err);
+    }
+
+    function actualizarFin() {
+        const boxInicio = document.getElementById("inicio");
+        const boxFin = document.getElementById("fin");
+        const value = parseInt(boxInicio.value);
+    
+        if (!isNaN(value)) {
+            boxFin.value = (value + 1).toString();
+        }
+        else {
+            boxFin.value = "";
+        }
+    }
+
+    useEffect(() => {
+        try {
+            fetch(`http://localhost:3001/mis-horarios/cargar/horarios/${id}`)
+                .then(checkStatus)  
+                .then(response => response.json())
+                .then(data => setHorarios(data))
+                .catch(handleError)
+        }
+        catch(error) {
+        }
+    }, []);
+
+    function guardarHorarios() {
+        var fecha = document.getElementById("dia").value;
+        var horaInicio = document.getElementById("inicio").value;
+        alert(horaInicio)
+
+        fetch(`http://localhost:3001/mis-horarios/guardar/${fecha}/${id}/${horaInicio}`)
+            .then(checkStatus)
+            .then(response => response.text())
+            .then(data => alert(data))
+            .catch(handleError)
+    }
+
+    function borrarRegistro(idHorario) {
+        fetch(`http://localhost:3001/mis-horarios/borrar/horario/${idHorario}`)
+            .then(checkStatus)
+            .then(response => response.text())
+            .then(data => alert(data))
+            .catch(handleError)
+        }   
+
+    function formataerFecha(fecha) {
+        const dateString = fecha;
+        const dateObject = new Date(dateString);
+
+        const year = dateObject.getUTCFullYear();
+        const month = dateObject.getUTCMonth() + 1;
+        const day = dateObject.getUTCDate();
+
+const formattedDate = `${day}/${month}/${year}`;
+
+        return formattedDate
+    }
+
     return (
         <div id='mis_horarios'>
             <div className="container-3">
-                <h1 className="title">Mis horarios</h1>
+                <p className="p-title">Mi horarios</p>
                 <hr className="hr"></hr>
-                <p className="p">Agregue sus horarios de disponibilidad durante la semana</p>
+                <p className="p">Agregue sus horarios de disponibilidad durante la semana</p>  
 
-                <input type="text" id="dia" className="f-input"/>
+                <input type="date" id="dia" className="f-input"/>
                 <label for="dia" className="f-label">Dia de semana</label>
 
-                <input type="text" id="inicio" className="f-input"/>
+                <input type="number" id="inicio" className="f-input" onInput={actualizarFin} min={7} max={21}/>
                 <label for="inicio" className="f-label">Hora inicio</label>
 
-                <input type="text" id="fin" className="f-input"/>
+                <input type="text" id="fin" className="f-input" readOnly/>
                 <label for="fin" className="f-label">Hora fin</label>
 
                 <input type="text" id="enlace" className="f-input"/>
                 <label for="enlace" className="f-label">Enlace de sesión</label>
 
-                <button id="btn-agregar">Agregar</button>
+                <button id="btn-agregar" onClick={guardarHorarios}>Agregar</button>
             </div>
 
             <div className="container-4">
-                <button className='nbtn'>1</button> Lunes de 08:00 a 10:00 am
-                <button className='xbtn'>X</button>
-                <br/>
-                <button className='nbtn'>2</button> Martes de 04:00 pm a 06:00 pm
-                <button className='xbtn'>X</button>
-                <br/>
-                <button className='nbtn'>3</button> Miércoles de 05:00 pm a 08:00 pm
-                <button className='xbtn'>X</button>
+            {horarios.map((horario, index) => (
+                <span>
+                    <button className='nbtn'>{index + 1}</button>
+                    <span>
+                        {formataerFecha(horario.fecha)}
+                    </span>
+                    {horario.Rangos.map(rango => (
+                        <span key={rango.id}>
+                            {" de "}
+                            {rango.hora_inicio}:00 a {rango.hora_fin}:00
+                        </span>
+                    ))}
+                    <button className='xbtn' onClick={() => borrarRegistro(horario.id)}>X</button>
+                    <br/>
+                </span> 
+            ))}
             </div>
         </div>
     );
